cf98a9dc926b1d8fc54e2fe38c0aef69c29ef43e
Danny <Danny@10.117.183.255>
2019-03-28 18:20:54 -0700

    Added my Name and SacState ID.

--- Lab 02/src/Hello.java
+++ Lab 02/src/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 		System.out.println("215108765 - This line was added by Andrew Shin.");
 		System.out.println("217600033 - This line was added by Gabriel Rodriguez.");
 		System.out.println("216674680 - This line was added by Ryan Lacanilao.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("217919417 - This line was added by Daniel Martinez.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
